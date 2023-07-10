0aca969304bd626c3866f7446b84916a82506bc6
husnain ali <husnainali24444@gmail.com>
2023-07-10 13:15:55 +0500

    import signup page component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import {useState} from 'react';
-
-function App() {
- const [count,setcounter]=useState(0)
- 
- 
+import Signup from './Signup/Signup';
+// import {useState} from 'react';
+// function App() {
+//  const [count,setcounter]=useState(0)
+//   return(
+//   <>
+//     <h1 >count{count}</h1>
+//     <button onClick={()=>{setcounter(count+1)}}>increment</button>
+//     <button onClick={()=>{setcounter(count-1)}}>decrement</button>
+//   </>
+//   );
+// }
+function App(){
   return(
-  <>
-    <h1 >count{count}</h1>
-    <button onClick={()=>{setcounter(count+1)}}>increment</button>
-    <button onClick={()=>{setcounter(count-1)}}>decrement</button>
-  </>
+    <div>
+    <Signup/>
+    </div>
   );
 }
 
